dd3eb759ec973a58eb2bcc408fa1844e4bb90ddd
danielchakaveh <60737810+danielchakaveh@users.noreply.github.com>
2020-02-06 11:59:57 +0000

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,33 +1,11 @@
 
 /**
- * Write a description of class Snake here.
+ * Class Snake - Represents a snake animal in the simulation
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Obed Ngigi, Daniel Chakaveh-Roberts
+ * @version 1.0
  */
 public class Snake extends Animal
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Snake
-     */
-    public Snake()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
