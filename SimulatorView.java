5e336db7b728d07924230bf78e17d6d46f21a773
Obed Ngigi <obedngigi@live.co.uk>
2020-02-22 16:18:48 +0000

    Added "WEATHER CONDITION"

--- SimulatorView.java
+++ SimulatorView.java
@@ -192,7 +192,7 @@ public class SimulatorView extends JFrame
      */
     private class FieldView extends JPanel
     {
-        private final int GRID_VIEW_SCALING_FACTOR = 7;
+        private final int GRID_VIEW_SCALING_FACTOR = 6;
 
         private int gridWidth, gridHeight;
         private int xScale, yScale;
